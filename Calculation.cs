f1931715167246d2f37bec185f10f27727b5c10e
balaramaiah <102573148+balaramaiah@users.noreply.github.com>
2022-04-18 13:59:35 +0530

    Update Calculation.cs
    Comments

--- Calculation.cs
+++ Calculation.cs
@@ -11,11 +11,13 @@ namespace Assignments
         public static void Main(String[] args)
         {
             Console.WriteLine("Enter a First Number: ");
-            int x = int.Parse(Console.ReadLine());
+            int x = int.Parse(Console.ReadLine());        //Reading input from user
 
             Console.WriteLine("Enter a Second Number: ");
             int y = int.Parse(Console.ReadLine());
 
+            //Here user decission for which operation perform 
+            
             Console.WriteLine("1:Addition");
             Console.WriteLine("2:Subtraction");
             Console.WriteLine("3:Multiplication");
@@ -26,16 +28,16 @@ namespace Assignments
             switch (z)
             {
                 case 1:
-                    Console.WriteLine("Addition of Two Numbers :" + (x + y));
+                    Console.WriteLine("Addition of Two Numbers :" + (x + y));           //addition operation
                     break;
                 case 2:
-                    Console.WriteLine("Substraction of Two Numbers :" + (x - y));
+                    Console.WriteLine("Substraction of Two Numbers :" + (x - y));       //Substraction operation
                     break;
                 case 3:
-                    Console.WriteLine("Multiplication of Two Numbers :" + (x * y));
+                    Console.WriteLine("Multiplication of Two Numbers :" + (x * y));     //Multiplication operation
                     break;
                 case 4:
-                    Console.WriteLine("Division of Two Numbers :" + (x / y));
+                    Console.WriteLine("Division of Two Numbers :" + (x / y));           //Division operation
                     break;
                 default:
                     Console.WriteLine("Choose frpm 1 to 6 :");
